07940979dd26a16d3bd2c19e51cd994dc41188c8
debugaman <debugflasher@yahoo.com.hk>
2024-08-31 14:09:34 +0800

    env and img_id support

--- app.js
+++ app.js
@@ -1,67 +1,43 @@
-// require modules
 require('dotenv').config();
 const uuid = require('uuid');
-const path = require('path');
 const axios = require("axios");
-
-const mimeTypes = {
-	'.html': 'text/html',
-	'.js': 'application/javascript',
-	'.json': 'application/json',
-	'.css': 'text/css',
-	'.png': 'image/png',
-	'.jpg': 'image/jpeg',
-	'.jpeg': 'image/jpeg',
-	'.gif': 'image/gif',
-	'.txt': 'text/plain',
-	// Add more mappings as needed
-};
-
 const express = require('express');
 
 const { S3Client, GetObjectCommand, PutObjectCommand } = require('@aws-sdk/client-s3');
 const { getSignedUrl } = require('@aws-sdk/s3-request-presigner');
 
-var https = require('follow-redirects').https;
-
 const puppeteer = require('puppeteer');
 const fullPageScreenshot = require('puppeteer-full-page-screenshot').default;
 
-// initialize app
 const app = express();
 
-// setup boilerplate
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// initialize puppeteer function
-(async () => {
-	// post request is made to render
+
+(async () => {	
 	app.get('/render', (req, res) => {
-		// make sure authorization is present
+		
+		var debug = false;
+
 		if (req.headers.authorization === process.env.AUTH_KEY) {
-			// make sure both html and css are included
 			try {
-				if ((req.body.html && req.body.css)) {
-					// generate image function
+				if (debug || (req.body.html && req.body.css)) {
 					(async () => {
 						const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--window-size=1200,1200'] });
-
-						// open new browser page
 						const page = await browser.newPage();
 
-						/*
-						var request = {
-							"html": "<div id='main'><div class='photo flow'></div><div class='brand flow'><div class='text'>成功建立房地產團隊的關鍵步驟</div></div></div>",
-							"css": "#main { width: 1200px; height: 1200px; } .flow { position: absolute; display: block; top: 0; bottom: 0; left: 0; right: 0; background-repeat: none; background-size: cover; background-position: center center; } .photo { top: 450px; background-image: url(https://images.pexels.com/photos/27299109/pexels-photo-27299109.jpeg); } .brand { background-image: url(https://louisjr-temp.s3.ap-east-1.amazonaws.com/aaverve/fb-1.png); } .text { text-align: center; margin-top: 180px; color: #FFF; font-family: Noto Sans HK, sans-serif; font-optical-sizing: auto; font-style: normal; font-size: 72px;}"
+						if(debug){							
+							var request = {
+								"html": "<div id='main'><div class='photo flow'></div><div class='brand flow'><div class='text'>成功建立房地產團隊的關鍵步驟</div></div></div>",
+								"css": "#main { width: 1200px; height: 1200px; } .flow { position: absolute; display: block; top: 0; bottom: 0; left: 0; right: 0; background-repeat: none; background-size: cover; background-position: center center; } .photo { top: 450px; background-image: url(https://images.pexels.com/photos/27299109/pexels-photo-27299109.jpeg); } .brand { background-image: url(https://louisjr-temp.s3.ap-east-1.amazonaws.com/aaverve/fb-1.png); } .text { text-align: center; margin-top: 180px; color: #FFF; font-family: Noto Sans HK, sans-serif; font-optical-sizing: auto; font-style: normal; font-size: 72px;}"
+							}
+	
+							req.body.html = request.html;
+							req.body.css = request.css;							
 						}
 
-						req.body.html = request.html;
-						req.body.css = request.css;
-						*/
-
 						await page.setViewport({ width: 1200, height: 1200 });
-						// fill content with user submitted html and css
 						await page.setContent(
 							`<style>
 								body {
@@ -73,36 +49,24 @@ app.use(express.json());
 								${req.body.html}
 							</div>`
 						);
-						// define content area to take screenshot
-						const content = await page.$('#container');
-						// take screenshot in content area, save buffer
-						//const buffer = await page.screenshot({ type: 'png', fullPage: true });
-
+						
 						var img_uuid = uuid.v4();
-						const buffer = await fullPageScreenshot(page, { path: 'img/' + img_uuid + '.png' });
-						//const buffer_2 = await page.screenshot({path: 'test.png', fullPage: true});
-						// close browser page
+						if(req.body.img_id){
+							img_uuid = req.body.img_id;
+						}
+
+						await fullPageScreenshot(page, { path: 'img/' + img_uuid + '.png' });
 						await page.close();
 
 						await browser.close();
 
 						var aws_s3_url = await upload_s3(__dirname + '/img/' + img_uuid + '.png', img_uuid + '.png');
-						//uploadFile(__dirname + '/img/' + img_uuid + '.png', img_uuid + '.png', "aacms");
-
-						// send back base64 string of image
-						var base64str = 'data:image/png;base64,' + base64_encode('img/' + img_uuid + '.png');
-						//console.log(base64str);
-						//res.status(200).send();
-						//res.sendFile(__dirname + '/img/' + img_uuid + '.png');
 						res.status(200).send({
-							"source_html": req.body.html,
-							"aws_s3_url": "https://aacms.s3.ap-southeast-1.amazonaws.com/" + img_uuid + '.png',
-							"img_url": req.protocol + '://' + req.get('host') + '/img/' + img_uuid + '.png'
+							"aws_s3_url": aws_s3_url						
 						});
 
 					})();
-				} else {
-					// if fields missing
+				} else {					
 					res.status(400).send('Some fields are missing with request.');
 				}
 			}
@@ -110,38 +74,23 @@ app.use(express.json());
 				res.status(400).send('Some fields are missing with request.');
 			}
 
-		} else {
-			// if no authorization present
+		} else {			
 			res.status(403).send('Auth key missing with request.');
 		}
 	});
 
-	app.get('/img/:imageName', (req, res) => {
-		const imageName = req.params.imageName;
-		const imagePath = __dirname + `/img/${imageName}`; // Replace with the actual path
-
-		res.sendFile(imagePath);
-	});
-
-	// function to encode file data to base64 encoded string
-	function base64_encode(file) {
-		var fs = require('fs');
-		return fs.readFileSync(file, 'base64');
-	}
-
-	async function upload_s3(file_path, file_name) {
-		// Configure AWS SDK V3
+	async function upload_s3(file_path, file_name) {		
 		const s3Client = new S3Client({
-			region: 'ap-southeast-1', // Change to your desired region
+			region: process.env.aws_region,
 			credentials: {
-				accessKeyId: "{{accessKeyId}}",
-				secretAccessKey: "{{secretAccessKey}}",
+				accessKeyId: process.env.aws_accessKeyId,
+				secretAccessKey: process.env.aws_secretAccessKey,
 			},
 		});
 
 		try {
 			const params = {
-				Bucket: 'aacms', // Your S3 bucket name
+				Bucket: process.env.aws_bucket, // Your S3 bucket name
 				Key: file_name, // Replace with your desired object key
 				ACL: 'public-read', // Set object to public-read
 			};
@@ -179,15 +128,12 @@ app.use(express.json());
 					console.log(error);
 				});
 
-
-
-			return "https://aacms.s3.ap-southeast-1.amazonaws.com/" + file_name;
+			return `https://${process.env.aws_bucket}.s3.${process.env.aws_region}.amazonaws.com/` + file_name;
 		} catch (error) {
 			console.error('Error generating pre-signed URL:', error);
 		}
 	}
 })();
 
-// listen to port
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log('Server started and running on port ' + PORT));
